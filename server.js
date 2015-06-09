1670cc881c87edeaebde9bd1fe7bb080e2fa0f7c
Liam Dickson <liam@Liams-MacBook-Air.local>
2015-06-09 00:34:12 -0400

    testing out express

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+var express = require('express');
+var app = express();
+
+// respond with "Hello World!" on the homepage
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+});
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Server listening at http://%s:%s', host, port);
+});
